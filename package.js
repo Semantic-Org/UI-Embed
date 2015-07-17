e9ba983153435fcb6aeca8b92b1bc4de8f51e2ce
Pusher Robot <jack@semantic-ui.com>
2015-07-17 16:49:24 -0400

    Updated component to version 2.0.4

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-embed',
   summary : 'Semantic UI - Embed: Single component release',
-  version : '2.0.3',
+  version : '2.0.4',
   git     : 'git://github.com/Semantic-Org/UI-Embed.git',
 });
 
