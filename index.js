ad70b0ff60535fa316e4f22b8c9130d327e70a83
Pusher Robot <jack@semantic-ui.com>
2015-08-10 14:44:05 -0400

    Updated component to version 2.0.8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * # Semantic UI 2.0.7 - Video
+ * # Semantic UI 2.0.8 - Video
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -536,7 +536,7 @@ module.exports = function(parameters) {
   ;
 };
 
-module.exports.settings = {
+_module.exports.settings = {
 
   name        : 'Embed',
   namespace   : 'embed',
