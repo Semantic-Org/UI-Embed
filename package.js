d2a38361794cc539f945911ea3a957559f390bf4
Jack Lukic <jack@semantic-ui.com>
2017-02-21 01:12:50 -0500

    Updated component to version 2.2.8

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-embed',
   summary : 'Semantic UI - Embed: Single component release',
-  version : '2.2.7',
+  version : '2.2.8',
   git     : 'git://github.com/Semantic-Org/UI-Embed.git',
 });
 
